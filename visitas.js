c0ead3fd4c4afb93c92906ac1b451f916e99b6ab
moreiralhenriqueglobo <moreiralhenrique@gmail.com>
2026-08-07 18:46:04 -0300

    Update visitas.js

--- visitas.js
+++ visitas.js
@@ -1,1 +1,8 @@
-const visitas = [];
+const visitas = [{
+    "data_visita": "data vista",
+    "hora_visita": "hora visita",
+    "qtd_visitas": 1
+  }
+];
+
+if (typeof module !== "undefined") { module.exports = visitas; }];
